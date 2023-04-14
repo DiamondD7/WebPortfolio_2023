4338c40927bee6d000f860b77abe478b882fdf94
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-14 23:46:17 +1200

    added menu overlay animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "./Hero";
 import { FiAlignJustify } from "react-icons/fi";
 
 import "../styles/homestyles.css";
 const Home = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
   return (
     <div>
+      <div className={menuClicked ? "overlay" : "overlayNone"}></div>
       <div className="hamburgermenu__wrapper">
-        <button className="btn-icon">
-          <FiAlignJustify size={"30px"} color={"#202020"} />
+        <button
+          className="btn-icon"
+          onClick={() => setMenuClicked(!menuClicked)}
+        >
+          <FiAlignJustify
+            size={"30px"}
+            color={menuClicked ? "#5e17eb" : "#202020"}
+          />
         </button>
       </div>
       <div className="hero-container">
