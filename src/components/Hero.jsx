73eb76812d4f48f4c879f45a6ef816431daec621
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-14 23:08:23 +1200

    initial commit: doing the menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Weblogo from "../assets/images/AJS-logo.png";
-import HeroAnimate from "../assets/images/Hero-animate.png";
+import Splatter from "../assets/images/splatter.png";
+import HeroCartoon from "../assets/images/Hero-cartoon.png";
 
 import "../styles/herostyles.css";
 const Hero = () => {
+  const NameText = <span style={{ color: "#5e17eb" }}>Aaron Sierra</span>;
   return (
     <div>
       <div className="hero-container__wrapper">
@@ -11,15 +13,22 @@ const Hero = () => {
           <div className="hero-texts_wrapper">
             <p className="hero-header__text">Welcome to my Portfolio.</p>
             <p className="hero-subheader__text">
-              Hello my name is Aaron Sierra, I am a Fullstack Web Developer.
-              This is where you'll find my latest and greatest web development
+              Hello my name is {NameText}, I am a Fullstack Web Developer. This
+              is where you'll find my latest and greatest web development
               projects. I hope you enjoy exploring and give me a little feedback
               after, Thank you
             </p>
+            <div className="hero-btns_wrapper">
+              <button className="hero-btn__about">About</button>
+              <button className="hero-btn__portfolio">Portfolio</button>
+            </div>
           </div>
-          <div className="hero-animate-image__wrapper">
-            <img className="hero-animate__image" src={HeroAnimate} />
+          <div className="hero-cartoon-image__wrapper">
+            <img className="hero-cartoon__image" src={HeroCartoon} />
           </div>
+        </div>
+        <div className="image-splatter">
+          <img src={Splatter} />
         </div>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "./Hero";
+import { FiAlignJustify } from "react-icons/fi";
 
 import "../styles/homestyles.css";
 const Home = () => {
   return (
     <div>
+      <div className="hamburgermenu__wrapper">
+        <button className="btn-icon">
+          <FiAlignJustify size={"30px"} color={"#202020"} />
+        </button>
+      </div>
       <div className="hero-container">
         <Hero />
       </div>
