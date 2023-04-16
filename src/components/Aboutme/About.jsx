07debf1476c9e213d9949b2c7195ebd1737ab014
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-16 20:23:42 +1200

    added the statements in the about component

--- src/components/Aboutme/About.jsx
+++ src/components/Aboutme/About.jsx
@@ -4,7 +4,18 @@ import "../../styles/aboutstyles.css";
 const About = () => {
   return (
     <div>
-      <div className="about-container__wrapper"></div>
+      <div className="about-container__wrapper">
+        <h1 className="about-header__text">
+          Hello, I am Aaron. Nice to meet you.
+        </h1>
+        <p className="about-paragraph__text">
+          I am New Zealand based Fullstack Web Developer, a passionate
+          individual that is always willing to learn necessary things to be
+          successful in this industry. I like to work with .Net Core using React
+          or Angular as the frontend. Over the years, I have been crafting my
+          skills to be one of the best dev in Auckland NZ.
+        </p>
+      </div>
     </div>
   );
 };
