c72d43edab929d20dccde5cfda061cf5a289bdeb
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-18 22:52:55 +1200

    implemented the hobbies section of the about component with animation but no context yet

--- src/components/Aboutme/About.jsx
+++ src/components/Aboutme/About.jsx
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import {
+  FiDribbble,
+  FiHeadphones,
+  FiTerminal,
+  FiEdit,
+  FiCamera,
+  FiMonitor,
+} from "react-icons/fi";
 
 import "../../styles/aboutstyles.css";
 const Description = () => {
@@ -117,11 +125,102 @@ const Description = () => {
 
           {hobbiesClicked && (
             <div
+              className="hobbies-container"
               style={
                 hobbiesClicked ? { animation: "opacityEffect .5s ease-in" } : {}
               }
             >
-              <p>Hello</p>
+              <div className="tx">
+                <h2 className="description-texts__header">Gaming</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiMonitor
+                  size={"30px"}
+                  color="#5e17eb"
+                  className="floatfloat"
+                />
+              </div>
+
+              <div className="tx">
+                <h2 className="description-texts__header">Sports</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiDribbble
+                  size={"30px"}
+                  color="#5e17eb"
+                  className="floatfloat"
+                />
+              </div>
+
+              <div className="tx">
+                <h2 className="description-texts__header">Music</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiHeadphones
+                  size={"30px"}
+                  color="#5e17eb"
+                  className="floatfloat"
+                />
+              </div>
+
+              <div className="tx">
+                <h2 className="description-texts__header">Photgraphy</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiCamera
+                  size={"30px"}
+                  color="#5e17eb"
+                  className="floatfloat"
+                />
+              </div>
+
+              <div className="tx">
+                <h2 className="description-texts__header">Designing</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiEdit size={"30px"} color="#5e17eb" className="floatfloat" />
+              </div>
+
+              <div className="tx">
+                <h2 className="description-texts__header">Coding</h2>
+                <p className="description-texts__subheader">
+                  I play too often! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ab veniam molestias dolore exercitationem
+                  odit ratione neque fuga quibusdam deleniti nihil illo
+                  laboriosam repellat, repellendus aspernatur, quaerat delectus
+                  laborum fugiat itaque.
+                </p>
+                <FiTerminal
+                  size={"30px"}
+                  color="#5e17eb"
+                  className="floatfloat"
+                />
+              </div>
             </div>
           )}
         </div>
