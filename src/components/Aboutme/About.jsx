beb0253a15ebe257b8a55953a39de7d81a58e6a7
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-18 20:33:05 +1200

    added animation when button is clicked on the description nav changed

--- src/components/Aboutme/About.jsx
+++ src/components/Aboutme/About.jsx
@@ -2,16 +2,46 @@ import React, { useState } from "react";
 
 import "../../styles/aboutstyles.css";
 const Description = () => {
+  const [skillsClicked, setSkillsClicked] = useState(true);
+  const [hobbiesClicked, setHobbiesClicked] = useState(false);
+
   return (
     <div>
       <div className="description__wrapper">
         <div className="description-list__wrapper">
           <ul>
             <li>
-              <button>Skills</button>
+              <button
+                onClick={() => {
+                  setSkillsClicked(true);
+                  setHobbiesClicked(false);
+                }}
+                style={
+                  skillsClicked === true
+                    ? {
+                        borderBottom: "1px solid #f3f3f3",
+                      }
+                    : {}
+                }
+              >
+                Skills
+              </button>
             </li>
             <li>
-              <button>Hobbies</button>
+              <button
+                className={hobbiesClicked === true ? "test" : ""}
+                onClick={() => {
+                  setHobbiesClicked(true);
+                  setSkillsClicked(false);
+                }}
+                style={
+                  hobbiesClicked === true
+                    ? { borderBottom: "1px solid #f3f3f3" }
+                    : {}
+                }
+              >
+                Hobbies
+              </button>
             </li>
             <li>
               <button>Experience</button>
@@ -22,59 +52,78 @@ const Description = () => {
           </ul>
         </div>
         <div className="description-container__wrapper">
-          <div className="description-texts__wrapper">
-            <div className="description-texts-container">
-              <h2 className="description-texts__header">Frontend</h2>
-              <p className="description-texts__subheader">
-                The significance of the client side of an application lies in
-                its ability to engage users with the website, and as such, I
-                place great importance on its development.
-              </p>
+          {skillsClicked && (
+            <div
+              className="description-texts__wrapper"
+              style={
+                skillsClicked ? { animation: "opacityEffect .5s ease-in" } : {}
+              }
+            >
+              <div className="description-texts-container">
+                <h2 className="description-texts__header">Frontend</h2>
+                <p className="description-texts__subheader">
+                  The significance of the client side of an application lies in
+                  its ability to engage users with the website, and as such, I
+                  place great importance on its development.
+                </p>
 
-              <p className="description-texts__title1">Technologies I use:</p>
-              <p>Html, Css, Javascript, Semantic-ui</p>
+                <p className="description-texts__title1">Technologies I use:</p>
+                <p>Html, Css, Javascript, Semantic-ui</p>
 
-              <p className="description-texts__title1">
-                Frontend Libraries/Framework:
-              </p>
-              <p>React & Angular</p>
+                <p className="description-texts__title1">
+                  Frontend Libraries/Framework:
+                </p>
+                <p>React & Angular</p>
 
-              <p className="description-texts__title1">Dev Tools:</p>
-              <p>
-                React-Icons, Figma, Sketch, Font Awesome, HeroIcons, Coolors
-              </p>
+                <p className="description-texts__title1">Dev Tools:</p>
+                <p>
+                  React-Icons, Figma, Sketch, Font Awesome, HeroIcons, Coolors
+                </p>
+              </div>
+              <div className="description-texts-container">
+                <h2 className="description-texts__header">Backend</h2>
+                <p className="description-texts__subheader">
+                  The client-side of an application is crucial in captivating
+                  users, and similarly, the backend-side is vital in providing
+                  an application with its purpose and bringing it to fruition.
+                </p>
+                <p className="description-texts__title1">Technologies I use:</p>
+                <p>C#, Javascript, TS , .Net Core, MSSQL</p>
+                <p className="description-texts__title1">
+                  Backend Libraries/Framework:
+                </p>
+                <p>ASP.NET & ExpressJS</p>
+                <p className="description-texts__title1">Dev Tools:</p>
+                <p>Postman, SwaggerUI, Axios</p>
+              </div>
+              <div className="description-texts-container">
+                <h2 className="description-texts__header">
+                  Currently Learning
+                </h2>
+                <p className="description-texts__subheader">
+                  technology and software development, is a personal passion of
+                  mine. Continual learning and growth within this industry are
+                  paramount to my success.
+                </p>
+                <p className="description-texts__title1">Improving on these:</p>
+                <p>Angular, TS, ExpressJS, Design</p>
+                <p className="description-texts__title1">Self-development:</p>
+                <p>Communication & Pair-Programming</p>
+                <p className="description-texts__title1">To learn next:</p>
+                <p>Vue, Wordpress, AI, Python</p>
+              </div>
             </div>
-            <div className="description-texts-container">
-              <h2 className="description-texts__header">Backend</h2>
-              <p className="description-texts__subheader">
-                The client-side of an application is crucial in captivating
-                users, and similarly, the backend-side is vital in providing an
-                application with its purpose and bringing it to fruition.
-              </p>
-              <p className="description-texts__title1">Technologies I use:</p>
-              <p>C#, Javascript, TS , .Net Core, MSSQL</p>
-              <p className="description-texts__title1">
-                Backend Libraries/Framework:
-              </p>
-              <p>ASP.NET & ExpressJS</p>
-              <p className="description-texts__title1">Dev Tools:</p>
-              <p>Postman, SwaggerUI, Axios</p>
+          )}
+
+          {hobbiesClicked && (
+            <div
+              style={
+                hobbiesClicked ? { animation: "opacityEffect .5s ease-in" } : {}
+              }
+            >
+              <p>Hello</p>
             </div>
-            <div className="description-texts-container">
-              <h2 className="description-texts__header">Currently Learning</h2>
-              <p className="description-texts__subheader">
-                technology and software development, is a personal passion of
-                mine. Continual learning and growth within this industry are
-                paramount to my success.
-              </p>
-              <p className="description-texts__title1">Improving on these:</p>
-              <p>Angular, TS, ExpressJS, Design</p>
-              <p className="description-texts__title1">Self-development:</p>
-              <p>Communication & Pair-Programming</p>
-              <p className="description-texts__title1">To learn next:</p>
-              <p>Vue, Wordpress, AI, Python</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
