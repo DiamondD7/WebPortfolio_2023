9dd86c763314f09c3de05d2bb569cdee64a9cc05
diamond <aaron.diamond2@yahoo.co.nz>
2023-04-20 20:51:14 +1200

    added the picture and the first project with mock data.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import NetF from "../../assets/ProjectImages/nf.png";
 
 import "../../styles/myprojectsstyles.css";
+
+const ProjectContents = () => {
+  return (
+    <div>
+      <div className="contents-container__wrapper">
+        <div className="contents__wrapper">
+          <div>
+            <img
+              className="content-project_one"
+              src={NetF}
+              alt="netflix-project"
+            />
+          </div>
+          <div className="content-project-sumamry__wrapper">
+            <p className="content-project-summary_one">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+              dignissimos enim. Sit deserunt dolorum est fuga ex magni veniam et
+              aut? Voluptate asperiores autem porro illo, illum minus facilis
+              repellendus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Projects = () => {
   return (
     <div>
@@ -12,6 +40,10 @@ const Projects = () => {
           molestias odit nobis quisquam qui, nesciunt fuga. Molestiae itaque at
           asperiores facilis.
         </p>
+      </div>
+
+      <div>
+        <ProjectContents />
       </div>
     </div>
   );
