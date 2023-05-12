da48de1eea68454445723f7f5e3d32effc48a4c9
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-12 22:12:14 +1200

    added also a responsiveness to stop the animation

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -274,6 +274,79 @@ const ProjectContents = () => {
             </div>
           </div>
         </div>
+
+        <div className="contents__wrapper">
+          <div>
+            <img
+              className="content-project_one"
+              src={NetF}
+              alt="netflix-project"
+            />
+          </div>
+          <div className="content-project-sumamry__wrapper">
+            <p className="content-project-summary_one">
+              I have cloned the Netflix application that has the ability to play
+              videos with a dynamic hero that always changes when the page is
+              refreshed and recieves data from the API provided by TMBD. This
+              application is built using ReactJS.
+            </p>
+            <div className="content-project__buttons oneBtns">
+              <a
+                className="content-btn__viewproject"
+                href="https://nfcloneweb.azurewebsites.net/"
+                target="_blank"
+              >
+                View project
+              </a>
+              <a
+                className="content-btn__viewcode"
+                href="https://github.com/DiamondD7/Netflix-clone"
+                target="_blank"
+              >
+                View code
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contents__wrapper marLeft">
+          <div>
+            <img
+              className="content-project_one"
+              src={ShopNZ}
+              alt="ShopLiftClient-project"
+            />
+          </div>
+          <div className="content-project-sumamry__wrapper">
+            <p className="content-project-summary_one">
+              This project is a personal e-commerce project consuming its own
+              node.js API. This is the client side of the website and there is
+              another admin side of the website where admins can perform CRUD
+              operations and control the data of the website. This was made
+              using the M-E-R-N Stack. Additionally, this project is made just
+              for myself and my learning, that is why it is only partially
+              completed. The 'Men' and 'Women' sections are the only ones
+              working and populated by the API. <br /> FYI: Wait approximately 2
+              mins for the data to load.
+            </p>
+            <div className="content-project__buttons twoBtns">
+              <a
+                className="content-btn__viewproject"
+                href="https://shopliftnzclient.azurewebsites.net/"
+                target="_blank"
+              >
+                View project
+              </a>
+              <a
+                className="content-btn__viewcode"
+                href="https://github.com/DiamondD7/shoplift_client"
+                target="_blank"
+              >
+                View code
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
