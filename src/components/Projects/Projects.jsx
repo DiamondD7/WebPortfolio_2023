2bc0caedd5a8559d5d2bc7dccf6158e55a2007fe
diamond <aaron.diamond2@yahoo.co.nz>
2023-08-19 00:21:51 +1200

    update: added another project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import ShopAdmin from "../../assets/ProjectImages/shopAdmin.png";
 import FinanceTracker from "../../assets/ProjectImages/financetrackerss.png";
 import ToDo from "../../assets/ProjectImages/todoscreenshot.png";
 import Vrooms from "../../assets/ProjectImages/vroomss.jpg";
+import Blogger from "../../assets/ProjectImages/Bloggerss.png";
 
 import "../../styles/myprojectsstyles.css";
 
@@ -42,6 +43,52 @@ const ProjectContents = () => {
                 target="_blank"
               >
                 View code
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contents__wrapper marLeft">
+          <div>
+            <img
+              className="content-project_one"
+              src={Blogger}
+              alt="Blogger-project"
+            />
+          </div>
+          <div className="content-project-sumamry__wrapper">
+            <p className="content-project-summary_one">
+              At the heart of this project is a user-centric approach, offering
+              a space where writing enthusiasts can effortlessly showcase their
+              skills. From captivating articles and insightful reports to
+              breaking news and personal narratives, our platform accommodates a
+              diverse range of content. Seamlessly integrating an intuitive
+              ReactJS frontend with a robust ASP.NET Core WebAPI backend, users
+              experience a fluid and engaging environment.
+            </p>
+            <br />
+            <br />
+            <div className="content-project__buttons sevenBtns">
+              <a
+                className="content-btn__viewproject"
+                href="https://youtu.be/LB9V9aeFJYc"
+                target="_blank"
+              >
+                View project
+              </a>
+              <a
+                className="content-btn__viewcode"
+                href="https://github.com/DiamondD7/blogger_client"
+                target="_blank"
+              >
+                View code
+              </a>
+              <a
+                className="content-btn__viewcode"
+                href="https://github.com/DiamondD7/blogger_server_2/tree/master"
+                target="_blank"
+              >
+                View backend code
               </a>
             </div>
           </div>
@@ -147,7 +194,7 @@ const ProjectContents = () => {
           </div>
         </div>
 
-        <div className="contents__wrapper marLeft">
+        {/* <div className="contents__wrapper marLeft">
           <div>
             <img
               className="content-project_one"
@@ -189,7 +236,7 @@ const ProjectContents = () => {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
 
         <div className="contents__wrapper marLeft">
           <div>
@@ -313,36 +360,43 @@ const ProjectContents = () => {
           <div>
             <img
               className="content-project_one"
-              src={ShopNZ}
-              alt="ShopLiftClient-project"
+              src={Blogger}
+              alt="Blogger-project"
             />
           </div>
           <div className="content-project-sumamry__wrapper">
             <p className="content-project-summary_one">
-              This project is a personal e-commerce project consuming its own
-              node.js API. This is the client side of the website and there is
-              another admin side of the website where admins can perform CRUD
-              operations and control the data of the website. This was made
-              using the M-E-R-N Stack. Additionally, this project is made just
-              for myself and my learning, that is why it is only partially
-              completed. The 'Men' and 'Women' sections are the only ones
-              working and populated by the API. <br /> FYI: Wait approximately 2
-              mins for the data to load.
+              At the heart of this project is a user-centric approach, offering
+              a space where writing enthusiasts can effortlessly showcase their
+              skills. From captivating articles and insightful reports to
+              breaking news and personal narratives, our platform accommodates a
+              diverse range of content. Seamlessly integrating an intuitive
+              ReactJS frontend with a robust ASP.NET Core WebAPI backend, users
+              experience a fluid and engaging environment.
             </p>
-            <div className="content-project__buttons twoBtns">
+            <br />
+            <br />
+            <div className="content-project__buttons sevenBtns">
               <a
                 className="content-btn__viewproject"
-                href="https://shopliftnzclient.azurewebsites.net/"
+                href="https://youtu.be/LB9V9aeFJYc"
                 target="_blank"
               >
                 View project
               </a>
               <a
                 className="content-btn__viewcode"
-                href="https://github.com/DiamondD7/shoplift_client"
+                href="https://github.com/DiamondD7/blogger_client"
                 target="_blank"
               >
                 View code
+              </a>
+              <a
+                className="content-btn__viewcode"
+                href="https://github.com/DiamondD7/blogger_server_2/tree/master"
+                target="_blank"
+              >
+                View backend code
               </a>
             </div>
           </div>
