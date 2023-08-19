078236c3b2cabfb3162616f19cebb3cf8e12ab91
diamond <aaron.diamond2@yahoo.co.nz>
2023-08-20 02:22:25 +1200

    added switch cases for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import Home from "./components/Home";
 
 import "./App.css";
+import AllProjects from "./components/Projects/AllProjects";
 const App = () => {
   const [changeTheme, setChangeTheme] = useState(false);
   const lightTheme = () => {
     setChangeTheme(!changeTheme);
   };
+
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home lightTheme={lightTheme} changeTheme={changeTheme} />;
+      break;
+    case "/AllProjects":
+      component = <AllProjects />;
+      break;
+    default:
+      console.log("Error in the switch case");
+  }
   return (
     <div className="App" id={changeTheme === false ? "light" : "dark"}>
-      <Home lightTheme={lightTheme} changeTheme={changeTheme} />
+      {/* <Home lightTheme={lightTheme} changeTheme={changeTheme} /> */}
+      {component}
     </div>
   );
 };
